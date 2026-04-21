bfcbca0773a42db93893f55e6a311704c0d2deb4
bleronbaftiu21 <bleronbaftiu2@gmail.com>
2026-04-21 23:10:59 +0200

    Added Entities for Booking, BookingItem, Ticket and TicketType

---
+++ Eventix.Domain/Entities/BookingItem.cs
@@ -0,0 +1,26 @@
+﻿using Eventix.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventix.Domain.Entities
+{
+    public class BookingItem : BaseEntity
+    {
+        public Guid BookingId { get; set; }
+        public Guid TicketTypeId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        public decimal Subtotal => Quantity * UnitPrice;
+
+
+        // Navigation properties
+        public Booking Booking { get; set; } = default!;
+        public TicketType TicketType { get; set; } = default!;
+        public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+    }
+}
