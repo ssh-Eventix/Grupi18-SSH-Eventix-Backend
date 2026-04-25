b3d7b10fea20c36f1dcddb5dcde53d0baaf7eed7
RigaZubaku <riga.zubaku@student.uni-pr.edu>
2026-04-25 14:47:41 +0200

    add Role and UserRole entities with relationships to User

---
+++ Eventix.Domain/Entities/Role.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Eventix.Domain.Common;
+
+namespace Eventix.Domain.Entities
+{
+
+    public class Role : TenantBaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+
+        public virtual ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
+
+    }
+
+}
