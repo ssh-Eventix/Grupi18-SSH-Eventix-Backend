1d2c65b1f73e036d4f23bd22bcc68cf8dc615685
Yllka <yllka.fejzullahu@student.uni-pr.edu>
2026-04-27 01:13:18 +0200

    EVENTIX-49 fix EventSession EF Core mapping and relationships

---
+++ Eventix.Application/DTOs/EventSession/EventSessionResponseDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Eventix.Application.DTOs.EventSessions;
+
+public class EventSessionResponseDTO
+{
+    public Guid Id { get; set; }
+    public Guid TenantId { get; set; }
+    public Guid EventId { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+
+    public Guid? SpeakerId { get; set; }
+
+    public DateTime CreatedAtUtc { get; set; }
+    public DateTime? UpdatedAtUtc { get; set; }
+}
--- Eventix.Domain/Entities/EventSession.cs
+++ Eventix.Domain/Entities/EventSession.cs
@@ -1,39 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using Eventix.Domain.Common;
 
 namespace Eventix.Domain.Entities
 {
-    public class EventSession
+    public class EventSession : TenantBaseEntity
     {
-        [Key]
-        public Guid Id { get; set; }
-
-        [Required]
         public Guid EventId { get; set; }
-
-        [ForeignKey(nameof(EventId))]
-        public Event Event { get; set; }
-
-        [Required]
-        [MaxLength(200)]
-        public string Title { get; set; }
-
-        public string Description { get; set; }
-
-        [Required]
-        public DateTime StartTime { get; set; }
-
-        [Required]
-        public DateTime EndTime { get; set; }
-
         public Guid? SpeakerId { get; set; }
 
-        [ForeignKey(nameof(SpeakerId))]
-        public Speaker Speaker { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public virtual Event Event { get; set; } = null!;
+        public virtual Speaker? Speaker { get; set; }
     }
 }
