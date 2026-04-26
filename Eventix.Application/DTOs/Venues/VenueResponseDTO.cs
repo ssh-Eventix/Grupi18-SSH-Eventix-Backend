0c40dc3f6c2422eca9df8440d7aa2e316b91d485
Viola Resyli <121452187+violavv123@users.noreply.github.com>
2026-04-26 11:03:09 +0200

    testing frontend and backend connection (#18)
    * fixed db context and added middleware
    
    * added servies and fixed provisioner
    
    * fixed middleware
    
    * venue response dto

--- Eventix.Application/DTOs/Venues/VenueResponseDTO.cs
+++ Eventix.Application/DTOs/Venues/VenueResponseDTO.cs
@@ -9,21 +9,12 @@ namespace Eventix.Application.DTOs.Venues
 
         public string Name { get; set; } = default!;
         public string Code { get; set; } = default!;
-        public string? Description { get; set; }
 
         public string AddressLine1 { get; set; } = default!;
-        public string? AddressLine2 { get; set; }
         public string City { get; set; } = default!;
-        public string? State { get; set; }
-        public string? PostalCode { get; set; }
         public string Country { get; set; } = default!;
 
-        public decimal? Latitude { get; set; }
-        public decimal? Longitude { get; set; }
-
         public int TotalCapacity { get; set; }
-        public bool HasSeatingMap { get; set; }
-        public string? SeatingMapImageUrl { get; set; }
 
         public bool IsIndoor { get; set; }
         public bool IsAccessible { get; set; }
