1a504838e34680b0424c0a1e353242b1a33c5638
Yllka Fejzullahu <yllka.fejzullahu@student.uni-pr.edu>
2026-05-08 09:43:54 +0200

    Feature/eventix 61 reviewandcheckin (#38)
    * entitetet
    
    * EVENTIX-60: implement CheckIn module (entity, service, repo, controller)
    
    * EVENTIX-61: implement Review and Notification
    
    ---------
    
    Co-authored-by: Viola Resyli <121452187+violavv123@users.noreply.github.com>

---
+++ Eventix.Application/DTOs/Notifications/CreateNotificationDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Eventix.Application.DTOs.Notifications;
+
+public class CreateNotificationDTO
+{
+    public Guid UserId { get; set; }
+    public Guid? EventId { get; set; }
+    public int Type { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+}
