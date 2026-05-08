366cd6aea65b771d48beb60c8284e1387cc2cee2
Viola Resyli <121452187+violavv123@users.noreply.github.com>
2026-05-08 14:34:05 +0200

    added public role and public user role (#40)
    * EVENTIX-35-Front and back communication
    
    * added public role and public user role

--- Eventix.Domain/Entities/PublicUser.cs
+++ Eventix.Domain/Entities/PublicUser.cs
@@ -9,6 +9,8 @@ namespace Eventix.Domain.Entities
         public string FullName { get; set; } = string.Empty;
         public bool IsSuperAdmin { get; set; } = false;
         public bool IsActive { get; set; } = true;
+
+        public ICollection<PublicUserRole> PublicUserRoles { get; set; } = new List<PublicUserRole>();
     }
 }
 
