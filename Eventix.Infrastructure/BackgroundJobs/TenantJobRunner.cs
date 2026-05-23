6a8621ef4a4c9b8b1c7830eed8b35dc24d5b22e9
Viola Resyli <violaresyli@gmail.com>
2026-05-23 19:53:10 +0200

    fixed background jobs

---
+++ Eventix.Infrastructure/BackgroundJobs/TenantJobRunner.cs
@@ -0,0 +1,40 @@
+﻿using Eventix.Application.Interfaces.Common;
+using Eventix.Infrastructure.Persistence.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Eventix.Infrastructure.BackgroundJobs;
+
+public class TenantJobRunner
+{
+    private readonly PublicDbContext _publicDbContext;
+    private readonly IServiceScopeFactory _scopeFactory;
+
+    public TenantJobRunner(
+        PublicDbContext publicDbContext,
+        IServiceScopeFactory scopeFactory)
+    {
+        _publicDbContext = publicDbContext;
+        _scopeFactory = scopeFactory;
+    }
+
+    public async Task RunForAllTenants(Func<IServiceProvider, Task> job)
+    {
+        var tenants = await _publicDbContext.Tenants
+            .Where(x => x.IsActive)
+            .ToListAsync();
+
+        foreach (var tenant in tenants)
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var tenantContext =
+                scope.ServiceProvider.GetRequiredService<ITenantContext>();
+
+            tenantContext.TenantId = tenant.Id;
+            tenantContext.SchemaName = tenant.SchemaName;
+
+            await job(scope.ServiceProvider);
+        }
+    }
+}
