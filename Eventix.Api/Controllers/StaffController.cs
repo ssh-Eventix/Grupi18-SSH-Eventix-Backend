3bb05436a13ed1087da4bdbcb4c3af2a11228261
bleronbaftiu21 <bleronbaftiu2@gmail.com>
2026-05-25 14:09:12 +0200

    fixed StaffPage for Tenant

---
+++ Eventix.Api/Controllers/StaffController.cs
@@ -0,0 +1,51 @@
+﻿using Eventix.Application.DTOs.Staff;
+using Eventix.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eventix.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class StaffController : ControllerBase
+    {
+        private readonly IStaffService _staffService;
+
+        public StaffController(IStaffService staffService)
+        {
+            _staffService = staffService;
+        }
+
+        [HttpGet]
+        [Authorize(Policy = "Permission:ViewUsers")]
+        public async Task<ActionResult<List<StaffResponseDTO>>> GetAll(CancellationToken ct)
+        {
+            var staff = await _staffService.GetAllAsync(ct);
+            return Ok(staff);
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "Permission:CreateUsers")]
+        public async Task<ActionResult<StaffResponseDTO>> Create(CreateStaffDTO dto, CancellationToken ct)
+        {
+            try
+            {
+                var staff = await _staffService.CreateAsync(dto, ct);
+                return CreatedAtAction(nameof(GetAll), new { id = staff.Id }, staff);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id:guid}/deactivate")]
+        [Authorize(Policy = "Permission:UpdateUsers")]
+        public async Task<IActionResult> Deactivate(Guid id, CancellationToken ct)
+        {
+            var result = await _staffService.DeactivateAsync(id, ct);
+            return result ? NoContent() : NotFound();
+        }
+    }
+}
---
+++ Eventix.Application/Interfaces/Services/IStaffService.cs
@@ -0,0 +1,16 @@
+﻿using Eventix.Application.DTOs.Staff;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventix.Application.Interfaces.Services
+{
+    public interface IStaffService
+    {
+        Task<List<StaffResponseDTO>> GetAllAsync(CancellationToken ct = default);
+        Task<StaffResponseDTO> CreateAsync(CreateStaffDTO dto, CancellationToken ct = default);
+        Task<bool> DeactivateAsync(Guid id, CancellationToken ct = default);
+    }
+}
