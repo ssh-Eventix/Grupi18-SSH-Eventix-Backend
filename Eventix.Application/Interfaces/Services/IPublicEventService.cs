3d02a659dcefd17ccc7e6c0e08a3d5b3045baa03
Yllka <yllka.fejzullahu@student.uni-pr.edu>
2026-05-26 18:04:29 +0200

    SCRUM-245 Add global public events retrieval

---
+++ Eventix.Application/Interfaces/Services/IPublicEventService.cs
@@ -0,0 +1,14 @@
+﻿using Eventix.Application.DTOs.Events;
+
+namespace Eventix.Application.Interfaces.Services;
+
+public interface IPublicEventService
+{
+    Task<List<EventResponseDTO>> GetAllPublicAsync(
+        string? search,
+        CancellationToken cancellationToken = default);
+
+    Task<EventResponseDTO?> GetPublicByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+}
